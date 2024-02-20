5160ab9b2a9b6dfc777ccc1de7dcfadd8fe843c7
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-02-21 00:00:28 +0700

    commit code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,2 @@
 <h1 style="text-align: center;"> Học Laravel tại unicode</h1>
-<?php 
-if (env ('APP_ENV')=='production'){
-    echo 'call api live';
-}else {
-    echo 'call api sandbox';
-}
-?>
+<a href="<?php echo route('admin.tintuc', ['id' => 'tin-tuc-the-giơi']); ?>"> Xem tin tức</a>
